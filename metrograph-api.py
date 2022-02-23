58e45f97cc7e89f9c6d19ae512c5d0730bdcbe34
e-hamza <hamza.elgarrab@gmail.com>
2022-02-23 22:58:37 +0100

    Improving API response (adding more insights)

--- metrograph-api.py
+++ metrograph-api.py
@@ -1,6 +1,6 @@
 from sanic import Sanic
 from sanic.request import Request
-from sanic.response import HTTPResponse, text
+from sanic.response import HTTPResponse, text, json
 from metrograph import task as MetroTask
 import os
 import uuid
@@ -24,19 +24,22 @@ async def index(request: Request) -> HTTPResponse:
     if not os.path.exists(app.config.uploads_path):
         os.makedirs(app.config.uploads_path)
 
-    task_file_name = str(uuid.uuid4())
+    task_uid = str(uuid.uuid4())
     
-    async with aiofiles.open(f"{app.config.uploads_path}/{task_file_name}.zip", 'wb') as f:
+    async with aiofiles.open(f"{app.config.uploads_path}/{task_uid}.zip", 'wb') as f:
         await f.write(request.files["task_package"][0].body)
     f.close()
 
-    _t = MetroTask(task_path = f"{app.config.uploads_path}/{task_file_name}.zip", python_version=language_version, flat_task_path=app.config.flat_tasks_path)
-    print(_t)
+    _t = MetroTask(task_path = f"{app.config.uploads_path}/{task_uid}.zip", python_version=language_version, flat_task_path=app.config.flat_tasks_path)
     _t.unpack()
     _t.prepare()
     _t.run()
 
-    return text("Task completed successfully!")
+    return json({
+        "status" : "success",
+        "message" : "Task started successfully",
+        "task_id" : _t.task_uid
+    })
 
 
 app.run(host='0.0.0.0', port=1337, access_log=False, fast=True)
